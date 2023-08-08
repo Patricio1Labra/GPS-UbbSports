ff6f3066de66e69bcf272fae0af109b3d849a08c
Benjamin Ortega <benjamin.ortega1801@alumnos.ubiobio.cl>
2023-08-08 20:15:11 +0000

    prueba push

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Inicio from './Benjamin/index.jsx';
+import SolicitarImplementos from './Benjamin/SolicitarImplementos.jsx';
+import CrearSolicitudRecursos from './Benjamin/CrearSolicitudRecuros.jsx';
+import GestionarSolicitudRecursos from './Benjamin/GestionarSolicitudRecursos.jsx';
 
-/* para poner rutas
 const App = () => {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={} />
+                <Route path="/" element={<Inicio />} />
+                <Route path="/solicitar-implementos" element={<SolicitarImplementos />} />
+                <Route path="/gestionar-recursos" element={<GestionarSolicitudRecursos />} />
+                <Route path="/solicitar-recursos" element={<CrearSolicitudRecursos />} />
             </Routes>
         </Router>
     );
 };
-*/
+
 export default App;
---
+++ src/app/Benjamin/GestionarSolicitudRecursos.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { Link } from 'react-router-dom';
+
+class Inicio extends Component{
+    render() {
+        return (
+            <div>
+                <h1>Gestionar solicitud de recursos</h1>
+            </div>
+        )
+    }
+}
+
+
+export default Inicio;
