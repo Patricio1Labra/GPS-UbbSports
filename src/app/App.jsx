19c3c442d3b9e4b5e5c37f4561a40aa0ff28dde4
LeoSanhue <103543183+LeoSanhue@users.noreply.github.com>
2023-08-08 20:02:18 +0000

    start branch

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-/* para poner rutas
+/* para poner rutas a
 const App = () => {
     return (
         <Router>
