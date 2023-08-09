5d21941996035bcab6e873a231f485ad3b7df106
Patricio1Labra <patricio.labra1801@alumnos.ubiobio.cl>
2023-08-09 10:01:59 -0400

    Revert "Add files via upload"
    This reverts commit 5955435829fdf3f19018664ba60d631ad2a14452.

--- database.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const URI = "mongodb://127.0.0.1:27017/GPS";
-
-mongoose.connect(URI)
-    .then( db => console.log("BD Conectada"))
-    .catch(err => console.error(err));
-
-module.exports = mongoose;
