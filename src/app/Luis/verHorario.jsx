274cf129209121dd95c8f1db3dc99a5e4c5a5d87
root <levasquez.-@hotmail.com>
2023-08-10 03:22:15 +0000

    Ver ramas inscritas y ver horarios listos - Luis

--- src/app/Luis/verHorario.jsx
+++ src/app/Luis/verHorario.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Modal from 'react-modal';
-
-import 'bootstrap/dist/css/bootstrap.min.css'; // Importa los estilos de Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/style.css';
 
 const VerHorario = ({ isOpen, closeModal, selectedRama }) => {
+    
+
     return (
         <Modal
             isOpen={isOpen}
@@ -11,18 +13,26 @@ const VerHorario = ({ isOpen, closeModal, selectedRama }) => {
             contentLabel="Ver Horario"
             ariaHideApp={false}
             className="modal-dialog modal-dialog-centered"
+            overlayClassName="modal-backdrop"
         >
             {selectedRama && (
-                <div>
-                    <h2 className="modal-title">{selectedRama.nombre}</h2>
-                    <p><strong>Día:</strong> {selectedRama.horario.dia}</p>
-                    <p><strong>Hora de Inicio:</strong> {selectedRama.horario.horaInicio}</p>
-                    <p><strong>Hora de Salida:</strong> {selectedRama.horario.horaSalida}</p>
-                    <p><strong>Recinto:</strong> {selectedRama.recinto}</p>
-                    <p><strong>Entrenador:</strong> {selectedRama.entrenador}</p>
-                    <button className="btn btn-secondary" onClick={closeModal}>
-                        Cerrar
-                    </button>
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title">{selectedRama.nombre}</h5>
+                        <button type="button" className="btn-close" onClick={closeModal}></button>
+                    </div>
+                    <div className="modal-body">
+                        <p><strong>Día:</strong> {selectedRama.horario.dia}</p>
+                        <p><strong>Hora de Inicio:</strong> {selectedRama.horario.horaInicio}</p>
+                        <p><strong>Hora de Salida:</strong> {selectedRama.horario.horaSalida}</p>
+                        <p><strong>Recinto:</strong> {selectedRama.recinto}</p>
+                        <p><strong>Entrenador:</strong> {selectedRama.entrenador}</p>
+                    </div>
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-secondary" onClick={closeModal}>
+                            Cerrar
+                        </button>
+                    </div>
                 </div>
             )}
         </Modal>
--- src/app/Luis/verRamaInscritas.jsx
+++ src/app/Luis/verRamaInscritas.jsx
@@ -1,14 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import DataTable from 'react-data-table-component';
 import axios from 'axios';
-import Modal from 'react-modal';
+import DataTable from 'react-data-table-component';
 
-
-
-
+import VerHorario from './verHorario.jsx';
 import { useSpring, animated } from 'react-spring';
-
-
+import Modal from 'react-modal';
 import Swal from 'sweetalert2';
 import { useNavigate } from 'react-router-dom';
 import AppBar from '@mui/material/AppBar';
@@ -17,83 +13,81 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-import "../Felipe/Home.css";
 import TemporaryDrawer from '../Felipe/Comps/TempEncargado.jsx';
-
-import VerHorario from './verHorario.jsx';
-
-import 'bootstrap/dist/css/bootstrap.min.css'; // Importa los estilos de Bootstrap
-import './assets/style.css'; // Importa el archivo CSS de estilos generales
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/style.css';
 import { useRadioGroup } from '@mui/material';
 
-const VerRamaInscritas = ({user,setUser}) => {
-    const [ramasInscritas, setRamasInscritas] = useState([]);
-    const [modalIsOpen, setModalIsOpen] = useState(false);
-    const [selectedRama, setSelectedRama] = useState(null);
-    const [estudiante, setEstudiante] = useState(null);
-    const estudianteId = '64d3544345d266a0d45a5f5c'; // ID del estudiante (ajusta esto)
+const VerRamaInscritas = ({ user, setUser }) => {
+  const [ramasInscritas, setRamasInscritas] = useState([]);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [selectedRama, setSelectedRama] = useState(null);
+  const [estudiante, setEstudiante] = useState(null);
+  const estudianteId = user._id; // Usamos el _id del estudiante que ha iniciado sesión
 
-    const handleLogout = () => {
-      setUser([]); // Reset user state
-      localStorage.removeItem('user'); // Clear user data from localStorage
-      window.location.href = '/'; // Redirect to login page
-    };
-    const [menuOpen, setMenuOpen] = useState("");
-    const handleMenuClick = () => {
-      setMenuOpen(!menuOpen);
-    };
+  const handleLogout = () => {
+    setUser([]); // Resetear el estado de usuario
+    localStorage.removeItem('user'); // Limpiar los datos del usuario del localStorage
+    window.location.href = '/'; // Redirigir a la página de inicio de sesión
+  };
+
+  const [menuOpen, setMenuOpen] = useState(false);
+  const handleMenuClick = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  useEffect(() => {
+    // Obtiene las ramas deportivas inscritas del estudiante
+    axios.get(`/api/estudiantes1/${user._id}/ramas`)
+        .then(response => {
+            setRamasInscritas(response.data);
+        })
+        .catch(error => {
+            console.error('Error al obtener las ramas inscritas:', error);
+        });
+
+    // Obtiene la información del estudiante
+    axios.get(`/api/estudiantes1/${user._id}`)
+        .then(response => {
+            setEstudiante(response.data);
+        })
+        .catch(error => {
+            console.error('Error al obtener la información del estudiante:', error);
+        });
+}, []);
   
 
-    useEffect(() => {
-        // Obtiene las ramas deportivas inscritas del estudiante
-        axios.get(`/api/estudiantes1/${estudianteId}/ramas`)
-            .then(response => {
-                setRamasInscritas(response.data);
-            })
-            .catch(error => {
-                console.error('Error al obtener las ramas inscritas:', error);
-            });
 
-        // Obtiene la información del estudiante
-        axios.get(`/api/estudiantes1/${estudianteId}`)
-            .then(response => {
-                setEstudiante(response.data);
-            })
-            .catch(error => {
-                console.error('Error al obtener la información del estudiante:', error);
-            });
-    }, []);
+  const columns = [
+    { name: 'Nombre de la Rama', selector: 'nombre', sortable: true },
+    {
+      name: 'Ver Horario',
+      cell: row => (
+        <button
+          className="btn btn-primary btn-sm"
+          onClick={() => openModal(row)}
+        >
+          Ver Horario
+        </button>
+      ),
+      ignoreRowClick: true,
+      allowOverflow: true,
+      button: true,
+    },
+  ];
 
-    const columns = [
-        { name: 'Nombre de la Rama', selector: 'nombre', sortable: true },
-        {
-            name: 'Ver Horario',
-            cell: row => (
-                <button
-                    className="btn btn-primary btn-sm"
-                    onClick={() => openModal(row)}
-                >
-                    Ver Horario
-                </button>
-            ),
-            ignoreRowClick: true,
-            allowOverflow: true,
-            button: true,
-        },
-    ];
+  const openModal = (rama) => {
+    setSelectedRama(rama);
+    setModalIsOpen(true);
+  };
 
-    const openModal = (rama) => {
-        setSelectedRama(rama);
-        setModalIsOpen(true);
-    };
+  const closeModal = () => {
+    setSelectedRama(null);
+    setModalIsOpen(false);
+  };
 
-    const closeModal = () => {
-        setSelectedRama(null);
-        setModalIsOpen(false);
-    };
-
-    return (
-      <div>
+  return (
+    <div>
       <Box sx={{ flexGrow: 1 }}>
         <AppBar position="static">
           <Toolbar className="barra" style={{ color: 'white' }}>
@@ -107,7 +101,7 @@ const VerRamaInscritas = ({user,setUser}) => {
               <TemporaryDrawer open={menuOpen} onClose={handleMenuClick} style={{ color: 'black' }} />
             </IconButton>
             <Typography className='texto1' variant="h6" style={{ color: 'white' }} component="div" sx={{ flexGrow: 1 }}>
-              <center><span style={{ color: 'white' }}>Bienvenido,</span> <span style={{ color: 'white' }}>{estudianteId.nombre}</span></center>
+              <center><span style={{ color: 'white' }}>Bienvenido,</span> <span style={{ color: 'white' }}>{user.nombre}</span></center>
             </Typography>
             <Button onClick={handleLogout} style={{ color: 'white' }} color="inherit">Cerrar Sesión</Button>
           </Toolbar>
@@ -121,24 +115,43 @@ const VerRamaInscritas = ({user,setUser}) => {
                 </div>
                 <div className="card-body">
                     {estudianteId && (
-                        <div>
-                            <p className="fs-6"><strong>Nombre:</strong> {estudianteId.nombre}</p>
-                            <p className="fs-6"><strong>Carrera:</strong> {estudianteId.carrera}</p>
-                            <p className="fs-6"><strong>Teléfono:</strong> {estudianteId.telefono}</p>
-                            <p className="fs-6"><strong>Correo:</strong> {estudianteId.correo}</p>
-                            <p className="fs-6"><strong>RUT:</strong> {estudianteId.rut}</p>
-                            <p className="fs-6"><strong>Descripción:</strong> {estudianteId.descripcion}</p>
+                    <div className="row">
+                        <div className="col-md-6">
+                        <p className="fs-6"><strong>Nombre:</strong> {user.nombre}</p>
+                        <p className="fs-6"><strong>Carrera:</strong> {user.carrera}</p>
+                        <p className="fs-6"><strong>Teléfono:</strong> {user.telefono}</p>
                         </div>
+                        <div className="col-md-6">
+                        <p className="fs-6"><strong>Correo:</strong> {user.correo}</p>
+                        <p className="fs-6"><strong>RUT:</strong> {user.rut}</p>
+                        <p className="fs-6"><strong>Descripción:</strong> {user.descripcion}</p>
+                        </div>
+                    </div>
                     )}
-                </div>
+                    </div>
             </div>
             <DataTable
-                columns={columns}
-                data={ramasInscritas}
-                pagination
-                highlightOnHover
-                className="mt-4 fs-6 table-responsive"
-            />
+        columns={columns}
+        data={ramasInscritas}
+        pagination
+        paginationPerPage={10}
+        paginationRowsPerPageOptions={[10, 20, 30]}
+        paginationComponentOptions={{
+          rowsPerPageText: 'Filas por página:',
+          rangeSeparatorText: 'de',
+          noRowsPerPage: false,
+          selectAllRowsItem: false,
+          selectAllRowsItemText: 'Todos',
+          nextIcon: 'Siguiente',
+          prevIcon: 'Anterior',
+        }}
+        paginationIconFirstPage={<span>&laquo;&laquo;</span>}
+        paginationIconLastPage={<span>&raquo;&raquo;</span>}
+        paginationIconNext={<span>&raquo;</span>}
+        paginationIconPrevious={<span>&laquo;</span>}
+        highlightOnHover
+        className="mt-4 fs-6 table-responsive"
+      />
             <VerHorario
                 isOpen={modalIsOpen}
                 closeModal={closeModal}
@@ -148,5 +161,4 @@ const VerRamaInscritas = ({user,setUser}) => {
     </div>    
     );
 };
-
 export default VerRamaInscritas;
